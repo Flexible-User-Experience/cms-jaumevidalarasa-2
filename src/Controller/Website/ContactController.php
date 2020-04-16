ce62f7cd02ee9970f16992cd39962025f35fcf4f
David Romaní <david@flux.cat>
2020-04-16 13:23:14 +0200

    create contact form

---
+++ src/Controller/Website/ContactController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Website;
+
+use App\Entity\Contact;
+use App\Form\Type\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends AbstractController
+{
+    public function page(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // TODO perform mail notification
+        }
+
+        return $this->renderView('contactform.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Entity/Contact.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+class Contact
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * Methods
+     */
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName(string $name): Contact
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return $this
+     */
+    public function setEmail(string $email): Contact
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     *
+     * @return $this
+     */
+    public function setMessage(string $message): Contact
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
